0f6810af96c897973d1051787643dff28bd47b83
michael jerome mambaje <themusick@michaels-mbp.gateway.2wire.net>
2014-05-16 18:33:50 -0700

    no message

--- Expressions_Industry/AssociatingJS/js/script.js
+++ Expressions_Industry/AssociatingJS/js/script.js
@@ -8,19 +8,19 @@
 var firstName = "Michael Jerome";
 var lastName = "Mambaje";
 var fullName = firstName + " " + lastName;
-const pay = 75;
-var minutes = 60;
-var dayTotal  = [75, 75, 75, 75, 75];
-var total = dayTotal[0] + dayTotal[1] + dayTotal[2] + dayTotal[3] + dayTotal[4];
-var totalHours = 5 * 14;
-totalHours = 70;
-var totalPay = totalHours * pay;
-totalPay = 5250;
-var perMinute = minutes * totalHours;
-perMinute = 4200;
-var minutePay = 5250 / 4200;
+const pay = 75; //Hourly rate.
+var minutes = 60; //Minutes per hour.
+var dayTotal  = [75, 75, 75, 75, 75]; //Arrays
+var total = dayTotal[0] + dayTotal[1] + dayTotal[2] + dayTotal[3] + dayTotal[4]; //Arrays
+var totalHours = 5 * 14; //Per day hourly multiplied by 2 weeks
+totalHours = 70; //Total hours
+var totalPay = totalHours * pay; //Total hours multiplied by $75 to get the final pay.
+totalPay = 5250; //Total payment for 2 weeks.
+var perMinute = minutes * totalHours; //Total hours of 70 multiplied by 60 minutes.
+perMinute = 4200; //Total minutes for 2 weeks of work.
+var minutePay = 5250 / 4200; //Total pay divided by total minutes to get the pay per 1 minute.
 console.log("They will pay " + fullName + " a total pay of " + totalPay + " for 2 weeks, " + total + " for 5 hours, " +
-     + minutePay + "per 1 minute. ");
+     + minutePay + " per 1 minute. "); //Final calculations.
 
 
 
