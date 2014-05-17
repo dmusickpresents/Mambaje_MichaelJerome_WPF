9fd569232af2c99c5bee55b8c16d47756f125ca7
michael jerome mambaje <themusick@michaels-mbp.gateway.2wire.net>
2014-05-16 17:53:55 -0700

    no message

--- Expressions_Industry/AssociatingJS/js/script.js
+++ Expressions_Industry/AssociatingJS/js/script.js
@@ -3,6 +3,7 @@
 //Expressions Industry
 
 //Calculate the cost to develop a website base on $75 per hour, 5 hours a day for 2 weeks.
+//Then calculate how much they're paying per min.
 
 var dayTotal  = [75, 75, 75, 75, 75];
 var total = dayTotal[0] + dayTotal[1] + dayTotal[2] +dayTotal[3] + dayTotal[4] + dayTotal[5];
@@ -10,6 +11,9 @@ var totalHours = 5 * 14;
 totalHours = 70;
 var totalPay = 75 * 70;
 totalPay = 5250;
+var perMinute = 60 * 70;
+perMinute = 4200;
+var minutePay = 5250 / 4200;
 
 
 
