ac82ff5a32eaef0184be18d3453625e1dbc07b4d
michael jerome mambaje <themusick@michaels-mbp.gateway.2wire.net>
2014-05-16 17:57:56 -0700

    no message

--- Expressions_Industry/AssociatingJS/js/script.js
+++ Expressions_Industry/AssociatingJS/js/script.js
@@ -2,9 +2,11 @@
 //05-15-2014
 //Expressions Industry
 
-//Calculate the cost to develop a website base on $75 per hour, 5 hours a day for 2 weeks.
+//Calculate how much they would pay Michael Jerome to develop a website base on $75 per hour, 5 hours a day for 2 weeks.
 //Then calculate how much they're paying per min.
 
+var firstName = "Michael Jerome";
+var lastName = "Mambaje";
 var dayTotal  = [75, 75, 75, 75, 75];
 var total = dayTotal[0] + dayTotal[1] + dayTotal[2] +dayTotal[3] + dayTotal[4] + dayTotal[5];
 var totalHours = 5 * 14;
@@ -14,6 +16,7 @@ totalPay = 5250;
 var perMinute = 60 * 70;
 perMinute = 4200;
 var minutePay = 5250 / 4200;
+
 
 
 
