f6b2a07f127b251f408e280f020055b18ab50b04
michael jerome mambaje <dmusickpresents@yahoo.com>
2014-05-22 11:57:42 -0700

    no message

--- Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/js/script.js
@@ -3,13 +3,13 @@
 //Conditionals worksheet
 
 var mpg = 45;
-var tankReading = 30;
-var gasCapacity = prompt("Enter your gas capacity:");
+var tankReading = 10;
+var gasCapacity = prompt("Enter your gas tank capacity in gallons:");
 
 if(tankReading > mpg){
 
     console.log("You can make it without stopping for gas!");
-}else{
+}else{(gasCapacity < tankReading);
 
     console.log("You only have " + gasCapacity + " gallons of gas in your tank," +
         "better get gas now while you can!");
