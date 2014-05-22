a98e89b5f0584d3eb032c2fef01b4600ddc129b3
michael jerome mambaje <dmusickpresents@yahoo.com>
2014-05-22 16:11:09 -0700

    no message

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -2,4 +2,12 @@
 //05-22-2014
 //Conditionals Personal
 
+var paycheck = 2600;
+var gucciSunglasses = 325;
+var bills = 2300;
 
+if()
+
+
+//if bills is less than 1800, I will buy sunglasses
+//if its more than 1800, I wont
