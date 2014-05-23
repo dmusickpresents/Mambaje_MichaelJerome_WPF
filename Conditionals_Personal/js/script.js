ddd759796ab1593a2975867b48d9e7faefab8dcf
michael jerome mambaje <dmusickpresents@yahoo.com>
2014-05-22 18:51:22 -0700

    no message

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -11,9 +11,9 @@ var billsToPayThisMonth = prompt("How much is total bills for this month?");
 
 //If and Else
 if(billsToPayThisMonth > priceForVacation){
-     console.log("You can't go to Hawaii!");
+     console.log("You can't go to Hawaii!"); //No Hawaii!
 }else{
-    console.log("You can go to Hawaii!");
+    console.log("You can go to Hawaii!"); //Mahalo!
 //Alert for pocket money or money left after calculations.
     alert(paycheckPlusSavings - billsToPayThisMonth - priceForVacation + " is your pocket money!");
 
