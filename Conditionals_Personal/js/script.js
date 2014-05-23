5f022fd62548164cd6a19c8d0f735048f735bd3b
michael jerome mambaje <dmusickpresents@yahoo.com>
2014-05-22 18:34:18 -0700

    no message

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -2,14 +2,22 @@
 //05-22-2014
 //Conditionals Personal
 
-var amountOfPaycheck = prompt("How much is your paycheck this month?");
+alert("To Hawaii or not?");
+
+//Prompts
+var paycheckPlusSavings = prompt("How much is your paycheck this month?");
 var priceForVacation = prompt("How much is vacation package for Hawaii?");
 var billsToPayThisMonth = prompt("How much is total bills for this month?");
 
+//If and Else
 if(billsToPayThisMonth > priceForVacation){
      console.log("We can't go to Hawaii.");
 }else{
     console.log("We can go to Hawaii.");
+
+    alert(paycheckPlusSavings - billsToPayThisMonth - priceForVacation);
+
+
 }
 
 
