29ec35bbb92aacdea163057bcb92bad72455a01b
michael jerome mambaje <dmusickpresents@yahoo.com>
2014-05-22 20:17:12 -0700

    no message

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -4,7 +4,12 @@
 
 alert("Limit of 10 pictures per person.");
 var guest = prompt("How many guest are you having?");
-var limitOfPictures = 500;
 
-limitOfPictures = (guest < 50) ? "I am charging $150" : "I am charging $250";
+
+var chargeIfLessThan50 = 150;
+var chargeIfMoreThan50 = 250;
+limitOfPictures = (guest < 50) ? "I am charging $" + chargeIfLessThan50 + "" : "I am charging $" + chargeIfMoreThan50
++ "";
 console.log(limitOfPictures);
+
+var nameOnPictures = prompt("would you like to put a title on your pictures?");
