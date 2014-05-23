afb9d95f3ed22a6679edc6756836bcc06a7d90fc
michael jerome mambaje <dmusickpresents@yahoo.com>
2014-05-22 19:56:20 -0700

    no message

---
+++ Conditionals_Wacky/js/script.js
@@ -0,0 +1,8 @@
+//Michael Jerome Mambaje
+//05-22-2014
+//Conditionals Wacky
+
+var guest = prompt("How many guest are you having?");
+var pictureTakingEveryPerson = 10;
+var limitOfPictures = 500;
+
