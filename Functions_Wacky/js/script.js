5da0012c2ff826984ff35047aa73edbfc13b2e29
michael jerome mambaje <dmusickpresents@yahoo.com>
2014-05-29 20:13:50 -0700

    no message

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -3,10 +3,10 @@
 //Functions Wacky
 
 //Arithmetic Operators
-alert("Calculation of Light years to Human years.");
+alert("Calculation of how fast a pitcher throws a baseball.");
 
 var distance = prompt("Please enter distance of the pitcher.");
 var secondsBallTravel = prompt("Please enter time ball traveled.");
 var pitchingSpeed = distance / secondsBallTravel;
-alert(pitchingSpeed);
-console.log(" the speed of the baseball is " + pitchingSpeed + " feet per second");
+alert("The speed of the baseball is " + pitchingSpeed + " feet per second.");
+console.log("The speed of the baseball is " + pitchingSpeed + " feet per second.");
