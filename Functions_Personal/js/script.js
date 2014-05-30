40a7c03257a941db5102a6afe0f0f0abb07ef42d
michael jerome mambaje <dmusickpresents@yahoo.com>
2014-05-29 18:14:43 -0700

    no message

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -13,5 +13,5 @@ var planeTaxiTotal = planeTicket + taxiTotal;
 
 var vegasTrip;
 
-vegasTrip = (planeTaxiTotal > driveCommuteTotal) ? "We will drive going to Vegas!" : "We will fly going to Vegas!";
+vegasTrip = (planeTaxiTotal < driveCommuteTotal) ? "We will drive going to Vegas!" : "We will fly going to Vegas!";
 console.log(vegasTrip)
