628119b4be45d484ec65fabb7ab489993a7c7ce3
michael jerome mambaje <dmusickpresents@yahoo.com>
2014-05-29 18:13:39 -0700

    no message

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -2,12 +2,16 @@
 //05-29-2014
 //Functions Personal
 
-var gasPrice = 399;
+var gasPrice = 3.99;
+var denaliTankTotal = 26;
+var totalGasPT = gasPrice * denaliTankTotal;
 var carRentalTotal = 280;
-var driveCommuteTotal = gasPrice * carRentalTotal;
-
+var driveCommuteTotal = totalGasPT * carRentalTotal;
+var planeTicket = 300;
+var taxiTotal = 150;
+var planeTaxiTotal = planeTicket + taxiTotal;
 
 var vegasTrip;
 
-
-vegasTrip = ()
+vegasTrip = (planeTaxiTotal > driveCommuteTotal) ? "We will drive going to Vegas!" : "We will fly going to Vegas!";
+console.log(vegasTrip)
