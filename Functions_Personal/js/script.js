48f8124c67d28ad85ccd5db2163e5ba476d058e2
michael jerome mambaje <dmusickpresents@yahoo.com>
2014-05-29 18:36:49 -0700

    no message

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -21,9 +21,10 @@ console.log(vegasTrip);
 
 var planeBagAllowedLbs = 50;
 var myLuggage = 51;
+var removeClothing = 49.99;
 
 if(myLuggage > planeBagAllowedLbs){
-    console.log("I can check in my bags.");
+    console.log("I can't check in my bags.");
 
 }else{
     console.log("I have to take out some clothes!");
