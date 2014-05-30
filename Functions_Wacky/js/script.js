d41b11b0dfb657a8f46a15232ec5fe60c3333809
michael jerome mambaje <dmusickpresents@yahoo.com>
2014-05-29 20:24:08 -0700

    no message

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -8,8 +8,9 @@ alert("Calculation of how fast a pitcher throws a baseball.");
 var distance = prompt("Please enter distance of the pitcher.");
 var secondsBallTravel = prompt("Please enter time ball traveled.");
 var pitchingSpeed = distance / secondsBallTravel;
-
+var feetPerSecond = 0.682;
+var ballSpeed = prompt(pitchingSpeed * feetPerSecond);
 //alert("The speed of the baseball is " + pitchingSpeed + " feet per second.");
-console.log("The speed of the baseball is " + pitchingSpeed + " feet per second.");
+//console.log("The speed of the baseball is " + pitchingSpeed + " feet per second.");
 
 
